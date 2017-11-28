f0dc6de7d51c096ede76d8d17960f9f0b5265b5c
sbmaxx <sbmaxx@yandex-team.ru>
2017-11-28 16:24:55 +0300

    initial

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const port = process.env.HTTP_PORT || 3333;
+const xmlPath = path.resolve(process.env.XML_FILE || 'turbo.xml');
+
+app.use(bodyParser.json());
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.get('/api/turbo', (req, res) => {
+    res.set('Content-Type', 'text/xml');
+    fs.createReadStream(xmlPath).pipe(res);
+});
+
+app.post('/api/turbo', (req, res) => {
+    if (!req.body.xml) {
+        return 'empty content';
+    }
+
+    fs.writeFile(xmlPath, req.body.xml, () => res.send('ok'))
+});
+
+app.listen(port, () => {
+    console.log(`Server started at http://localhost:${port}`);
+}).on('error', e => console.log(e));
